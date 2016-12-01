56727fd5f73d899c5d4e7e94b302a03702317938
WenChieh <wenchiehcm@gmail.com>
2016-12-01 14:36:29 +0800

    add implementation of calculator without using stack

--- calculator.cpp
+++ calculator.cpp
@@ -1,6 +1,6 @@
 class calculator{
 
-    int calc(string& str, int& curIdx){
+    int calc1(string& str, int& curIdx){
         stack<int> stk;
         int num = 0;
         char sign = '+';
@@ -15,7 +15,7 @@ class calculator{
             }
             else if(str[curIdx] == '('){
                 curIdx++;//discard '('
-                num = calc(str, curIdx);
+                num = calc1(str, curIdx);
                 fromInnerBracket = true;//indicating that ")" is from inner bracket
             }
             
@@ -49,9 +49,51 @@ class calculator{
         return num;
     }
     
+    /*Calculator not using stack*/
+    int calc2(string& str, int& curIdx){
+        int result = 0, curNum = 0, prevNum = 0;
+        int prevOprtr = '+';
+        int size = str.size();
+        while(curIdx < size){
+            if(isdigit(str[curIdx])){
+                /*Calculate current number. If previous operator is a '*' or '/', combine current number with previous number.*/
+                
+                curNum = 0;
+                while(curIdx < size && isdigit(str[curIdx]))
+                    curNum += curNum * 10 + (str[curIdx++] - '0');
+                
+                if(prevOprtr == '-') curNum = -curNum;
+                else if(prevOprtr == '*') curNum = prevNum * curNum;
+                else if(prevOprtr == '/') curNum = prevNum / curNum;
+                
+                curIdx--;//make curIdx point at the last digit of current number
+            }
+            else if(str[curIdx] == '('){
+                curIdx++;//discard the index of '('
+                curNum = calc2(str, curIdx);
+                if(prevOprtr == '-') curNum = -curNum;
+                else if(prevOprtr == '*') curNum = prevNum * curNum;
+                else if(prevOprtr == '/') curNum = prevNum / curNum;
+            }
+            else if(str[curIdx] == ')'){
+                break;
+            }
+            else if(!isspace(str[curIdx])){
+                if(str[curIdx] == '+' || str[curIdx] == '-')    result += curNum;
+                else                                            prevNum = curNum;
+                curNum = 0;
+                prevOprtr = str[curIdx];
+            }
+            
+            curIdx++;
+        }
+        return result + curNum;
+    }
 public:
     int calc(string& str){
         int curIdx = 0;
-        return calc(str, curIdx);
+        /*optional:
+        calc2(str, curIdx);*/
+        return calc1(str, curIdx);
     }
 };
